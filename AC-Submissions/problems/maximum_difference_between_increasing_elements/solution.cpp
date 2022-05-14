2b0845f58e59c046638dca5817dfa71e56adf007
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-05-14 12:11:05 +0000

    Sync LeetCode submission - Maximum Difference Between Increasing Elements (cpp)

---
+++ AC-Submissions/problems/maximum_difference_between_increasing_elements/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+    	int n=nums.size(),Min=1e9,Max=0;
+        vector<int>right(n),left(n);
+        for(int i=0; i<n; i++){
+        	Min=min(Min,nums[i]);
+        	right[i]=Min;
+        }
+        for(int i=n-1; i>=0; i--){
+        	Max=max(Max,nums[i]);
+        	left[i]=Max;
+        }
+        int res=-1;
+        for(int i=0; i<n; i++){
+        	if(right[i]<left[i]){
+        		res=max(res,left[i]-right[i]);
+        	}
+        }
+        return res;
+    }
+};
