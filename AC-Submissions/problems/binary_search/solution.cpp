e88386943f174aeccfe68eb9cf55e4cf72e958bb
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-05-16 17:43:09 +0000

    Sync LeetCode submission - Binary Search (cpp)

---
+++ AC-Submissions/problems/binary_search/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int L=0,R=nums.size()-1;
+        while(L<=R){
+            int mid=L+(R-L)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                R=mid-1;
+            }
+            else{
+                L=mid+1;
+            }
+        }
+        return -1;
+    }
+};
