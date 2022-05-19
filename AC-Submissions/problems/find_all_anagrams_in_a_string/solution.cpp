a2fd961f5fc31a185af7c2fa76f12feeb8634c84
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-05-19 09:23:35 +0000

    Sync LeetCode submission - Find All Anagrams in a String (cpp)

---
+++ AC-Submissions/problems/find_all_anagrams_in_a_string/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int plen=p.size();
+        vector<int>cnt(26,0);
+        for(int i=0; i<plen; i++){
+            cnt[p[i]-'a']++;
+        }
+        int slen=s.size(),L=0;
+        vector<int>currCnt(26,0),res;
+        if(plen>slen){return res;}
+        for(int i=0; i<slen; i++){
+            currCnt[s[i]-'a']++;
+            if(i>=plen){
+                currCnt[s[L]-'a']--;
+                L++;
+            }
+            int ok=1;
+            for(int j=0; j<26; j++){
+                if(currCnt[j]!=cnt[j]){
+                    ok=0; break;
+                }
+            }
+            if(ok==1){
+                res.push_back(L);
+            }
+        }
+        return res;
+    }
+};
