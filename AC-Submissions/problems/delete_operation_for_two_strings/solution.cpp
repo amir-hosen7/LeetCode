ec17f024f4ca8a73a7041d437f0f854208ce0aaa
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-06-03 08:59:33 +0000

    Sync LeetCode submission - Delete Operation for Two Strings (cpp)

---
+++ AC-Submissions/problems/delete_operation_for_two_strings/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int dp[510][510];
+    int solve(int i, int j, string &s, string &w){
+        if(i==s.size()){
+            if(j!=w.size()){
+                return w.size()-j;
+            }
+            return 0;
+        }
+        if(j==w.size()){
+            if(i!=s.size()){
+                return s.size()-i;
+            }
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans;
+        if(s[i]==w[j]){
+            ans=solve(i+1, j+1, s, w);
+        }
+        else{
+            int aa=1+solve(i+1, j, s, w);
+            int bb=1+solve(i, j+1, s, w);
+            ans=min(aa,bb);
+        }
+        return dp[i][j]=ans;
+    }
+    int minDistance(string word1, string word2) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0, 0, word1, word2);
+    }
+};
