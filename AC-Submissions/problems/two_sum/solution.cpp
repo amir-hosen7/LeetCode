985fddc993cb600d1acf97a64dae1501078f8610
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-06-06 07:07:56 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- AC-Submissions/problems/two_sum/solution.cpp
+++ AC-Submissions/problems/two_sum/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int l = nums.size();
-        vector <int> v; int flag = 0;
-        for(int i = 0; i < l; i++){
-            for(int j = i+1; j < l; j++){
-                if(nums[j] == (target-nums[i])){
-                    v.push_back(i); v.push_back(j);
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag) break;
+        vector<int>res;
+        int n=nums.size();
+        unordered_map<int, int>idx;
+        for(int i=0; i<n; i++){
+            idx[nums[i]]=i;
         }
-        return v;
+        for(int i=0; i<n; i++){
+            int currVal=target-nums[i];
+            if(idx[currVal]>i){
+                res.push_back(i);
+                res.push_back(idx[currVal]);
+                break;
+            }
+        }
+        return res;
     }
 };
