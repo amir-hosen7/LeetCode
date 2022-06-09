35db5120fa74af8717c93a63ffd9757d4a553361
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-06-09 11:02:40 +0000

    Sync LeetCode submission - Divisor Game (cpp)

--- AC-Submissions/problems/divisor_game/solution.cpp
+++ AC-Submissions/problems/divisor_game/solution.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    bool divisorGame(int n){
-        if(n&1){
-            return false;
-        }
-        else{
-            return true;
-        }
+   
+    // bool divisorGame(int n) {
+    //     bool dp[n+5];
+    //     dp[1]=false, dp[2]=true;
+    //     for(int i=3; i<=n; i++){
+    //         dp[i]=!dp[i-1];
+    //     }
+    //     return dp[n];
+    // }
+    
+     bool divisorGame(int n) {
+        return (n%2==0)?true:false;
     }
 };
