64df6d6cc68abb6cc4b9d304e3eb91a62c203929
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-06-14 02:47:56 +0000

    Sync LeetCode submission - Delete Operation for Two Strings (cpp)

--- AC-Submissions/problems/delete_operation_for_two_strings/solution.cpp
+++ AC-Submissions/problems/delete_operation_for_two_strings/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int dp[510][510];
-    int solve(int i, int j, string &s, string &w){
-        if(i==s.size()){
-            if(j!=w.size()){
-                return w.size()-j;
+    int dp[510][510], n, m;
+    int solve(int i, int j, string& s, string& w){
+        if(i==n){
+            if(j==m){
+                return 0;
             }
-            return 0;
+            return m-j;
         }
-        if(j==w.size()){
-            if(i!=s.size()){
-                return s.size()-i;
+        if(j==m){
+            if(i==n){
+                return 0;
             }
-            return 0;
+            return n-i;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
@@ -22,14 +22,21 @@ public:
             ans=solve(i+1, j+1, s, w);
         }
         else{
-            int aa=1+solve(i+1, j, s, w);
-            int bb=1+solve(i, j+1, s, w);
-            ans=min(aa,bb);
+            int ans1=1+solve(i+1, j, s, w);
+            int ans2=1+solve(i, j+1, s, w);
+            ans=min(ans1, ans2);
         }
         return dp[i][j]=ans;
     }
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
+        n=word1.size(), m=word2.size();
+        for(int i=0; i<510; i++){
+            for(int j=0; j<510; j++){
+                dp[i][j]=-1;
+            }
+        }
         return solve(0, 0, word1, word2);
     }
 };
+
+
