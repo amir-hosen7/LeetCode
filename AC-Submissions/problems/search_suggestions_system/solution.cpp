b9961d172964a5234cb54ec89206fa644d5fb103
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-06-19 13:54:20 +0000

    Sync LeetCode submission - Search Suggestions System (cpp)

--- AC-Submissions/problems/search_suggestions_system/solution.cpp
+++ AC-Submissions/problems/search_suggestions_system/solution.cpp
@@ -1,47 +1,55 @@
-class node{
+class Solution {
 public:
-	node *next[30];
-	vector<string>list;
-	node(){
-		for(int i=0; i<26; i++){
-			next[i]=NULL;
-		}
-	}
-}*root;
-
-class Solution{
-public:
-	vector<vector<string>>suggestedProducts(vector<string>&products, string searchWord){
-        root=new node();
-        int len=products.size();
-        sort(products.begin(),products.end());
-        for(int i=0; i<len; i++){
-        	node *curr=root;
-			int len=products[i].size();
-			for(int j=0; j<len; j++){
-				int id=products[i][j]-'a';
-				if(curr->next[id]==NULL){
-					curr->next[id]=new node();
-				}
-				curr=curr->next[id];
-				curr->list.push_back(products[i]);
-			}
+    struct ListNode{
+        vector<string>list;
+        ListNode *next[30];
+        ListNode(){
+            for(int i=0; i<=26; i++){
+                next[i]=NULL;
+            }
         }
-
-        len=searchWord.size();
-        node *curr=root;
-        vector<vector<string>>res(len);
-        for(int i=0; i<len; i++){
-        	int id=searchWord[i]-'a';
+    }*root;
+    
+    void insert(string &s){
+        int n=s.size();
+        ListNode *curr=root;
+        for(int i=0; i<n; i++){
+            int id=s[i]-'a';
+            if(curr->next[id]==NULL){
+                curr->next[id]=new ListNode();
+            }
+            curr=curr->next[id];
+            if(curr->list.size()<3){
+                curr->list.push_back(s);
+            }
+        }
+    }
+    
+    vector<vector<string>> query(string &s){
+        int n=s.size(),idx=0;
+        ListNode *curr=root;
+        vector<vector<string>>res(n);
+        for(int i=0; i<n; i++){
+            int id=s[i]-'a';
             if(curr->next[id]==NULL){
                 break;
             }
-        	curr=curr->next[id];
-        	int siz=curr->list.size();
-        	for(int j=0; j<(min(3,siz)); j++){
-        		res[i].push_back(curr->list[j]);
-        	}
+            curr=curr->next[id];
+            res[i]=curr->list;
         }
         return res;
     }
+    
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        root=new ListNode();
+        int n=products.size();
+        sort(products.begin(), products.end());
+        for(int i=0; i<n; i++){
+            insert(products[i]);
+        }
+        return query(searchWord);
+    }
 };
+
+
+
