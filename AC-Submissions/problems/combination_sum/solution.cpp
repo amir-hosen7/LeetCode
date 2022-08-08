35f50ac0e84cb0a04c4586c025c006bbd445c5b4
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-08-08 17:03:44 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- AC-Submissions/problems/combination_sum/solution.cpp
+++ AC-Submissions/problems/combination_sum/solution.cpp
@@ -3,23 +3,23 @@ public:
     vector<int> curr;
     vector<vector<int>> ans;
     void solve(int idx, int n, int t, vector<int>& v) {
-        if(idx>=n) {
-            return;
-        }
         if(t<=0) {
             if(t==0) {
                 ans.push_back(curr);
             }
             return;
         }
-        solve(idx+1, n, t, v);
-        curr.push_back(v[idx]);
-        solve(idx, n, t-v[idx], v);
-        curr.pop_back();
+        for(int i=idx; i<n; i++) {
+            curr.push_back(v[i]);
+            solve(i, n, t-v[i], v);
+            curr.pop_back();
+        }
     }
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n=candidates.size();
         solve(0, n, target, candidates);
         return ans;
     }
 };
+
