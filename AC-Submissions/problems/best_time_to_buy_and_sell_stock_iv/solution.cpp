fced218e6b235807a60d32592ac28938c0a06bd0
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-09-10 09:04:16 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (cpp)

--- AC-Submissions/problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
+++ AC-Submissions/problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int dp[1010][110][2];
-    int solve(int idx, int currK, int n, int k, vector<int>& v, int status){
-        if(idx>=n or currK>k){
-            return 0;
-        }
-        if(dp[idx][currK][status]!=-1){
-            return dp[idx][currK][status];
-        }
-        int profit;
+    int solve(int idx, int n, int k, vector<int>& v, int status){
+        if(idx>=n or k<=0) return 0;
+        int &ret = dp[idx][k][status];
+        if(ret!=-1) return ret;
+        int ans=0;
         if(status){
-            profit=max(v[idx]+solve(idx+1, currK, n, k, v, 0), solve(idx+1, currK, n, k, v, 1));
+            ans=max(v[idx] + solve(idx+1, n, k-1, v, 0), solve(idx+1, n, k, v, 1));
         }
         else{
-            int profit1=solve(idx+1, currK+1, n, k, v, 1)-v[idx];
-            int profit2=solve(idx+1, currK, n, k, v, 0);
-            profit=max(profit1, profit2);
+            ans=max(solve(idx+1, n, k, v, 1) - v[idx], solve(idx+1, n, k, v, 0));
         }
-        return dp[idx][currK][status]=profit;
+        return ret=ans;
     }
+    
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
         memset(dp, -1, sizeof(dp));
-        return solve(0, 0, n, k, prices, 0);
+        int n=prices.size();
+        return solve(0, n, k, prices, 0);
     }
 };
+
