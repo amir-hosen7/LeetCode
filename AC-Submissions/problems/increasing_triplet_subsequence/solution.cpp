17c4df27fab1037618661648e335b27fedb8441c
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-10-11 13:58:41 +0000

    Sync LeetCode submission - Increasing Triplet Subsequence (cpp)

--- AC-Submissions/problems/increasing_triplet_subsequence/solution.cpp
+++ AC-Submissions/problems/increasing_triplet_subsequence/solution.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int n=nums.size(), a=INT_MAX, b=INT_MAX;
+        int A1=INT_MAX, A2=INT_MAX;
+        int n=nums.size();
         for(int i=0; i<n; i++){
-            if(nums[i]<=a) a=nums[i];
-            else if(nums[i]<=b) b=nums[i];
-            else return true;
+            if(A1>nums[i]){
+                A1=nums[i];
+            }
+            if(nums[i]<A2){
+                if(nums[i]>A1){
+                    A2=nums[i];
+                }
+            }
+            if(nums[i]>A2){
+                return true;
+            }
         }
         return false;
     }
 };
-
-
