9df94f6fa87319311c51afe23be0a6d42f8caab9
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-10-22 11:25:30 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- AC-Submissions/problems/minimum_window_substring/solution.cpp
+++ AC-Submissions/problems/minimum_window_substring/solution.cpp
@@ -1,62 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-    	if(s.size()<t.size()){return "";}
-        int tlen=t.size();
-        vector<int>cnt(55,0);
-        for(int i=0; i<tlen; i++){
-        	int id=t[i]-'a'+26;
-        	if(t[i]>='A' and t[i]<='Z'){
-        		id=t[i]-'A';
-        	}
-        	cnt[id]++;
+        vector<int> cnt(60, 0);
+        int slen=s.size();
+        for(auto i:t){
+            int id=i-'a';
+            if(i>='A' and i<='Z'){
+                id=i-'A'+26;
+            }
+            cnt[id]++;
         }
-        int slen=s.size(),ok=0;
-        int L=0,R=slen,a=0,b;
-        vector<int>currCnt(55);
-        while(L<=R){
-        	int mid=L+(R-L)/2,flag=0,LL=0;
-        	for(int i=0; i<=52; i++){
-        		currCnt[i]=0;
-        	}
-        	for(int i=0; i<slen; i++){
-        		int id=s[i]-'a'+26;
-        		if(s[i]>='A' and s[i]<='Z'){
-        			id=s[i]-'A';
-        		}
-        		currCnt[id]++;
-        		if(i>=mid){
-        			int id=s[LL]-'a'+26;
-	        		if(s[LL]>='A' and s[LL]<='Z'){
-	        			id=s[LL]-'A';
-	        		}
-	        		currCnt[id]--;
-	        		LL++;
-        		}
-        		int ck=1;
-        		for(int j=0; j<=52; j++){
-        			if(currCnt[j]<cnt[j]){
-        				ck=0; break;
-        			}
-        		}
-        		if(ck==1){
-        			a=LL,b=i;
-        			flag=1; break;
-        		}
-        	}
-        	if(flag==1){
-        		ok=1;
-        		R=mid-1;
-        	}
-        	else{
-        		L=mid+1;
-        	}
+        vector<int> curr(60, 0);
+        int L=0, Min=INT_MAX, idx=-1;
+        for(int i=0; i<slen; i++){
+            int id=s[i]-'a';
+            if(s[i]>='A' and s[i]<='Z'){
+                id=s[i]-'A'+26;
+            }
+            curr[id]++;
+            bool ok=1;
+            while(ok){
+                for(int j=0; j<60; j++){
+                    if(cnt[j]>curr[j]){
+                        ok=0; break;
+                    }
+                }
+                if(ok){
+                    if((i-L+1)<Min){
+                        Min=i-L+1, idx=L;
+                    }
+                    int id=s[L]-'a';
+                    if(s[L]>='A' and s[L]<='Z'){
+                        id=s[L]-'A'+26;
+                    }
+                    curr[id]--, L++;
+                }
+            }
         }
-        if(ok==0){return "";}
-        t="";
-        for(int i=a; i<=b; i++){
-        	t+=s[i];
-        }
-        return t;
+        if(idx==-1) return "";
+        return s.substr(idx, Min);
     }
 };
+
