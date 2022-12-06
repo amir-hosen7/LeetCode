c4a390cd688c95fc20f702a0f7fd7630473158d6
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2022-12-06 20:25:06 +0000

    Sync LeetCode submission - N-Queens II (cpp)

--- AC-Submissions/problems/n-queens_ii/solution.cpp
+++ AC-Submissions/problems/n-queens_ii/solution.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    int totalNQueens(int n) {
-	vector<vector<bool>> board(n, vector<bool>(n, false));
-	return solve(board, 0);
+    int board[12], res;
+
+    bool isSafe(int row, int col) {
+        for (int i = 1; i <= col; i++) {
+            if (board[i] == row) return false;
+            int x = abs(row - board[i]), y = col - i;
+            if (x == y) return false;
+        }
+        return true;
     }
-    bool Check(vector<vector<bool>>& board, int row, int col) {
-	    int n = size(board);
-	    for(int i = 0; i <= row; i++) {
-		    if(board[i][col]) return false; 
-		    // checking if all diagonals are safe -
-		    if(row - i >= 0 && col - i >= 0 && board[row - i][col - i]) return false;
-		    if(row - i >= 0 && col + i <  n && board[row - i][col + i]) return false;
-	    }
-	    return true;
-    }    
-    int solve(vector<vector<bool>>& board, int row) {
-	    if(row == size(board)) return 1;
-	    int count = 0;
-	    for(int col = 0; col < size(board); col++)           
-		    if(Check(board, row, col)){          // check if we can place at (row, col)
-			    board[row][col] = true;          // place the queen at (row, col)
-			    count += solve(board, row + 1);  
-			    board[row][col] = false;         
-		    }                                
-	    return count;
+
+    void NQueens(int col, int n) {
+        if (col > n){
+            res++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (isSafe(i, col)) {
+                board[col] = i;
+                NQueens(col + 1, n);
+                board[col] = 0;
+            }
+        }
+    } 
+    int totalNQueens(int n) {
+        NQueens(1, n);
+        return res;
     }
 };
+
+
