3d74f0014b80efeaf1384c77228a55adbd069611
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2023-01-29 21:18:37 +0000

    Sync LeetCode submission - Cheapest Flights Within K Stops (cpp)

--- AC-Submissions/problems/cheapest_flights_within_k_stops/solution.cpp
+++ AC-Submissions/problems/cheapest_flights_within_k_stops/solution.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int dp[110][110];
-    vector<pair<int, int>> adj[110];
-    long long solve(int src, int dst, int cnt, int k) {
-        if (src == dst or (cnt - 1) >= k) {
-            if ((cnt - 1) > k or src != dst) return INT_MAX;
-            return 0;
-        } 
-        int &ret = dp[src][cnt];
-        if (ret != -1) return ret;
-        long long siz = adj[src].size(), ans = INT_MAX;
-        for (int i = 0; i < siz; i++) {
-            int v = adj[src][i].first, c = adj[src][i].second;
-            ans = min(ans, c + solve(v, dst, cnt + 1, k));
+    static const int N = 110;
+    vector<vector<int>> adj[N];
+    int solve(int n, int src, int dst, int k) {
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        queue<vector<int>> q;
+        q.push({src, 0, 0});
+        while (!q.empty()) {
+            vector<int> u = q.front();
+            int root = u[0], stops = u[1], cost = u[2];
+            q.pop();
+            if (stops > k) continue;
+            int siz = adj[root].size();
+            for (int i = 0; i < siz; i++) {
+                int child = adj[root][i][0], edgeCost = adj[root][i][1];
+                if (dist[child] > (cost + edgeCost) and stops <= k) {
+                    dist[child] = cost + edgeCost;
+                    q.push({child, stops + 1, dist[child]});
+                }
+            }
         }
-        return ret = ans;
+        if (dist[dst] >= 1e9) return -1;
+        return dist[dst];
     }
 
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
@@ -22,13 +30,7 @@ public:
         for (int i = 0; i < siz; i++) {
             adj[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
-        for (int i = 0; i <= n + 5; i++) {
-            for (int j = 0; j <= k + 5; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        int res = solve(src, dst, 0, k);
-        if (res >= INT_MAX) res = -1;
+        int res = solve(n, src, dst, k);
         return res;
     }
 };
