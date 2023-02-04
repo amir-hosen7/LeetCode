122bf8c4ce94dedf9e5899e7334ae972b21704ab
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2023-02-04 05:52:37 +0000

    Sync LeetCode submission - Permutation in String (cpp)

--- AC-Submissions/problems/permutation_in_string/solution.cpp
+++ AC-Submissions/problems/permutation_in_string/solution.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int len1=s1.size(),len2=s2.size();
-        vector<int>cnt(26,0),currCnt(26,0);
-        for(int i=0; i<len1; i++){
-        	cnt[s1[i]-'a']++;
+        vector<int> v(30, 0), cnt(30, 0);
+        for (auto i : s1) {
+            v[i - 'a' + 1]++;
         }
-        int L=0,ok;
-        for(int i=0; i<len2; i++){
-        	currCnt[s2[i]-'a']++;
-        	ok=1;
-        	for(int j=0; j<26; j++){
-        		if(cnt[j]!=currCnt[j]){
-        			ok=0; break;
-        		}
-        	}
-        	if(ok==1){break;}
-        	while(currCnt[s2[i]-'a']>cnt[s2[i]-'a']){
-        		currCnt[s2[L]-'a']--; L++;
-        	}
+        int siz = s1.size(), n = s2.size(), l = -1, flag = 0;
+        for (int i = 0; i < n; i++) {
+            if (i >= (siz - 1)) {
+                cnt[s2[i] - 'a' + 1]++;
+                if (l != -1) {
+                    cnt[s2[l] - 'a' + 1]--;
+                }
+                bool ok = 1;
+                for (int j = 1; j <= 26; j++) {
+                    if (cnt[j] != v[j]) {
+                        ok = 0; break;
+                    }
+                }
+                if (ok) {
+                    flag = 1; break;
+                }
+                l++;
+            }
+            else {
+                cnt[s2[i] - 'a' + 1]++;
+            }
         }
-        return ok;
+        return flag;
     }
 };
