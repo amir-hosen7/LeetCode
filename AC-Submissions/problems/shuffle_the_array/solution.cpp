945eafe19e91cba7c57a465cc63ed15c7bd3f965
Amantu Amir <42311923+amantuamir@users.noreply.github.com>
2023-02-06 10:04:05 +0000

    Sync LeetCode submission - Shuffle the Array (cpp)

--- AC-Submissions/problems/shuffle_the_array/solution.cpp
+++ AC-Submissions/problems/shuffle_the_array/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>res;
-        for(int i=0; i<n; i++){
-            res.push_back(nums[i]);
-            res.push_back(nums[i+n]);
-        }
-        return res;
+        vector<int> v(2*n);
+        int idx = 0;
+        for (int i = 0, j = n; i < n; i++, j++) {
+            v[idx++] = nums[i];
+            v[idx++] = nums[j];
+            }
+        return v;
     }
 };
